5336b066361e81a0e8adae860cb5a373ca44fbc9
imaldon4 <31159328+imaldon4@users.noreply.github.com>
2018-08-31 11:49:23 -0700

    Amost done

--- BlackjackGame/src/blackjackgame/BlackjackGame.java
+++ BlackjackGame/src/blackjackgame/BlackjackGame.java
@@ -28,14 +28,44 @@ public class BlackjackGame {
         answer = input.nextLine();
         yesNo = answer.charAt(0);
         
-        while(hand <= 21){
-            //prompt if wants to hit
-            //generate a card number
-            //display card number
-            //add card to hand --> if hand is over 21 display bust
-            //exit while loop
-            //prompt if want to try again.
-            //reset hand to zero
+        while(yesNo == 'y'){
+            while(hand <= 21){
+                int card;
+                int total = 0;
+                
+                //provide user with first card, then display
+                card = random.nextInt(11);
+                total += card;  //start aggregating the cards
+                System.out.println("Your first card is: " + card);
+                
+                //provide user with the second card, then display
+                card = random.nextInt(11);
+                total += card;  //aggregating cards
+                System.out.println("Your second card is: " + card);
+                System.out.println("Both cards total: " + total);
+                
+                System.out.println("");
+                System.out.println("Would you like to hit or stay? (h/s)");
+                answer = input.nextLine();
+                yesNo = answer.charAt(0);
+                
+                if (yesNo == 'h'){
+                    card = random.nextInt(11);
+                    total += card;
+                    if (total > 21){
+                        System.out.println("BUST!");
+                    }
+                    else {
+                        System.out.println("Cards total: " + total);
+                        System.out.println("Would you like to hit or stay? (h/s)");
+                    }
+                }
+                else {
+                    //thank you for playing
+                }
+              
+        }
+    
         }
     }
     
